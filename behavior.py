2b3a4938359e6e703c8b025aa4759bfd0c5636ca
famesensor <famfew25@gmail.com>
2021-01-28 22:23:17 +0700

    :hammer: init value and add import data

--- behavior.py
+++ behavior.py
@@ -1,18 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 23 21:16:47 2021
-
-@author: famfe
-"""
-
-import keras,os
+import keras
+import os
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
-trdata = ImageDataGenerator()
-traindata = trdata.flow_from_directory(directory="data",target_size=(224,224))
-tsdata = ImageDataGenerator()
-testdata = tsdata.flow_from_directory(directory="test", target_size=(224,224))
+# init value
+img_height, img_weight = 224, 244
+channels = 3  # 3 RGB
+train_data_path = "/data-set/behavior/train"
+validation_data_path = "/data-set/behavior/validate"
+batch_size = 512
 
+# TODO: Add batch-size and import test dataset
+trian_datagen = ImageDataGenerator(zoom_range=0.2, horizontal_flip=True)
+train_data = trian_datagen.flow_from_directory(directory=train_data_path, target_size=(
+    img_height, img_weight), subset="training", class_mode="categorical")
+validation_data = trian_datagen.flow_from_directory(directory=validation_data_path, target_size=(
+    img_height, img_weight), subset="validation", class_mode="categorical")
