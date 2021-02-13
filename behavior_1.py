f2037d6c4dc6231176b8c54f418169bfb79cdf26
famesensor <sathit.ju@outlook.com>
2021-02-13 16:52:46 +0700

    :hammer: update import lib, add import test-set and addplot graph

--- behavior_1.py
+++ behavior_1.py
@@ -1,7 +1,11 @@
+# TODO: refactor code now to function base
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+from datetime import datetime
 from keras.callbacks import ReduceLROnPlateau
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from datetime import datetime
-import keras
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Dense, Flatten
 from keras import optimizers
@@ -13,7 +17,6 @@ from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-import numpy as np
 
 # download vgg16 weight
 # https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
@@ -23,22 +26,25 @@ img_height, img_weight = 224, 244
 channels = 3  # 3 RGB
 train_data_path = "./data-set/behavior/train"
 validation_data_path = "./data-set/behavior/validate"
+test_data_path = "./data-set/behavior/test"
 weight_model_path = "./model/behavior/vgg16_weights"
 batch_size = 32
 Epochs = 18
-# classes_name = "binary"
-# classes_dict = ["eat", "noeat"]
+# dict_Label = {0: "eat", 1: "noeat"}
 
-# TODO: import test dataset
 trian_datagen = ImageDataGenerator(zoom_range=0.2, horizontal_flip=True)
 train_data = trian_datagen.flow_from_directory(directory=train_data_path, target_size=(
     img_height, img_weight), subset="training", class_mode="categorical", batch_size=batch_size)
 validation_data = trian_datagen.flow_from_directory(directory=validation_data_path, target_size=(
     img_height, img_weight), subset="validation", class_mode="categorical", batch_size=batch_size)
 
+test_datagen = ImageDataGenerator(zoom_range=0.2, horizontal_flip=True)
+test_data = test_datagen.flow_from_directory(
+    directory=test_data_path, target_size=(img_height, img_weight), batch_size=batch_size, class_mode='categorical')
+
 STEP_SIZE_TRAIN = train_data.n//train_data.batch_size
 STEP_SIZE_VALID = validation_data.n//validation_data.batch_size
-# STEP_SIZE_TEST = test_set.n//test_set.batch_size
+STEP_SIZE_TEST = test_data.n//test_data.batch_size
 
 # # Model VGG16_1
 vggmodel = VGG16(include_top=False, weights='imagenet',
@@ -82,9 +88,19 @@ history = model.fit_generator(train_data,
 
 duration = datetime.now() - start
 print("Training completed in time: ", duration)
-model.save_weights("vgg_16_behavior_1.5")
+model.save_weights("vgg_16_behavior_1.h5")
 
 # evalute model
-# score = model.evaluate(test_set)
-# print('Test Loss:', score[0])
-# print('Test accuracy:', score[1]
+score = model.evaluate(test_data)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+plt.plot(history.history["acc"])
+plt.plot(history.history['val_acc'])
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title("model accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend(["Accuracy", "Validation Accuracy", "loss", "Validation Loss"])
+plt.show()
--- behavior_2.py
+++ behavior_2.py
@@ -1,7 +1,11 @@
+# TODO: refactor code now to function base
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+from datetime import datetime
 from keras.callbacks import ReduceLROnPlateau
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from datetime import datetime
-import keras
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Dense, Flatten
 from keras import optimizers
@@ -13,7 +17,6 @@ from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-import numpy as np
 
 # download vgg16 weight
 # https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
@@ -23,11 +26,10 @@ img_height, img_weight = 224, 244
 channels = 3  # 3 RGB
 train_data_path = "./data-set/behavior/train"
 validation_data_path = "./data-set/behavior/validate"
+test_data_path = "./data-set/behavior/test"
 weight_model_path = "./model/behavior/vgg16_weights"
 batch_size = 32
 Epochs = 18
-# classes_name = "binary"
-# classes_dict = ["eat", "noeat"]
 
 # TODO: import test dataset
 trian_datagen = ImageDataGenerator(zoom_range=0.2, horizontal_flip=True)
@@ -36,16 +38,19 @@ train_data = trian_datagen.flow_from_directory(directory=train_data_path, target
 validation_data = trian_datagen.flow_from_directory(directory=validation_data_path, target_size=(
     img_height, img_weight), subset="validation", class_mode="categorical", batch_size=batch_size)
 
+test_datagen = ImageDataGenerator(zoom_range=0.2, horizontal_flip=True)
+test_data = test_datagen.flow_from_directory(
+    directory=test_data_path, target_size=(img_height, img_weight), batch_size=batch_size, class_mode='categorical')
+
 STEP_SIZE_TRAIN = train_data.n//train_data.batch_size
 STEP_SIZE_VALID = validation_data.n//validation_data.batch_size
-# STEP_SIZE_TEST = test_set.n//test_set.batch_size
+STEP_SIZE_TEST = test_data.n//test_data.batch_size
 
 # # Model VGG16_2
 vggmodel = VGG16(include_top=True, weights='imagenet',
                  input_tensor=None, input_shape=None)
 
 for layers in (vggmodel.layers)[:19]:
-    # print(layers)
     layers.trainable = False
 
 X = vggmodel.layers[-2].output
@@ -69,9 +74,19 @@ history = model.fit_generator(train_data, steps_per_epoch=STEP_SIZE_TRAIN, epoch
 
 duration = datetime.now() - start
 print("Training completed in time: ", duration)
-model.save_weights("vgg_16_behavior_2.5")
+model.save_weights("vgg_16_behavior_2.h5")
 
 # evalute model
-# score = model.evaluate(test_set)
-# print('Test Loss:', score[0])
-# print('Test accuracy:', score[1]
+score = model.evaluate(test_data)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+plt.plot(history.history["acc"])
+plt.plot(history.history['val_acc'])
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title("model accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend(["Accuracy", "Validation Accuracy", "loss", "Validation Loss"])
+plt.show()
