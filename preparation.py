12734ce6ae5daf77cea84e5e174631d4dc09b138
famesensor <sathit.ju@outlook.com>
2021-02-27 16:46:38 +0700

    :hammer: add load one image

--- preparation.py
+++ preparation.py
@@ -1,4 +1,8 @@
+import numpy as np
+import numpy as np
+
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
 
 
 def load_data_set(datagen_args: dict, data_set_path: str, size_image: tuple, batch_size: float):
@@ -6,3 +10,12 @@ def load_data_set(datagen_args: dict, data_set_path: str, size_image: tuple, bat
     data_set = data_generator.flow_from_directory(
         directory=data_set_path, target_size=size_image, class_mode="catrgorical", batch_size=batch_size)
     return data_set
+
+
+def load_image(test_data_path: str, size_image: tuple, color_mode: str):
+    test_image = image.load_img(
+        test_data_path, color_mode=color_mode, target_size=size_image)
+
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    return test_image
