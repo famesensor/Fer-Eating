0ad217d14383f31afe6c22ec3e9208746ba794a6
famesensor <sathit.ju@outlook.com>
2021-02-27 17:19:29 +0700

    :hammer: fix name

--- preparation.py
+++ preparation.py
@@ -8,7 +8,7 @@ from keras.preprocessing import image
 def load_data_set(datagen_args: dict, data_set_path: str, size_image: tuple, batch_size: float):
     data_generator = ImageDataGenerator(**datagen_args)
     data_set = data_generator.flow_from_directory(
-        directory=data_set_path, target_size=size_image, class_mode="catrgorical", batch_size=batch_size)
+        directory=data_set_path, target_size=size_image, class_mode="categorical", batch_size=batch_size)
     return data_set
 
 
