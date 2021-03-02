338de4103b490d186aa7da6b7b0cf84e46fc2cfd
famesensor <sathit.ju@outlook.com>
2021-03-03 00:29:18 +0700

    :hammer: update path and fix parameter

--- behavior_main.py
+++ behavior_main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     validation_data_path = "./data-set/behavior/validate"
     test_data_path = "./data-set/behavior/test"
     test_one_path = "./data-set/behavior/test/test.jpg"
-    check_point_path = "./model/behavior/vgg16_weights"
+    check_point_path = "./model/behavior/"
     save_weight_one_path = "./models/behavior/vgg16_behavior_1.h5"
     save_weight_two_path = "./models/behavior/vgg16_behavior_2.h5"
     batch_size = 32
@@ -53,17 +53,17 @@ if __name__ == "__main__":
     model_2 = init_model_vgg16(
         include_top=True, input_shape=None, input_tensor=None)
     model_2 = setup_architechture_vgg16_2(model_2)
-    model_train_2, history_2 = train_model_vgg16(check_point_path=check_point_path, save_weights_path=save_weight_two_path, model=model_2,
-                                                 train_data=train_data, validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs=Epochs)
+    model_train_2, history_2 = train_model_vgg16(checkpoint_path=check_point_path, save_weights_path=save_weight_two_path, model=model_2,
+                                                 train_data=train_data, validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs_train=Epochs)
     plot_result_train_model(history_2)
 
-    # test model
-    test_image = load_image(
-        test_one_path, (img_height, img_weight), color_mode='rgb')
-    result_model_1 = model_train_1.predict(test_image)
-    result_model_2 = model_train_2.predict(test_image)
+    # # test model
+    # test_image = load_image(
+    #     test_one_path, (img_height, img_weight), color_mode='rgb')
+    # result_model_1 = model_train_1.predict(test_image)
+    # result_model_2 = model_train_2.predict(test_image)
 
-    res_model_1 = np.argmax(result_model_1)
-    res_model_2 = np.argmax(result_model_2)
-    print("The predicted output model1 is :", dict_label[res_model_1])
-    print("The predicted output model2 is :", dict_label[res_model_2])
+    # res_model_1 = np.argmax(result_model_1)
+    # res_model_2 = np.argmax(result_model_2)
+    # print("The predicted output model1 is :", dict_label[res_model_1])
+    # print("The predicted output model2 is :", dict_label[res_model_2])
