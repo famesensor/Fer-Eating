dd9d14ec3d0786ca3ae18615e66b3ec9ee5ae150
famesensor <sathit.ju@outlook.com>
2021-03-06 18:18:47 +0700

    :hammer: fix img_weight, add path

--- behavior-detector.py
+++ behavior-detector.py
@@ -9,12 +9,13 @@ from keras.preprocessing import image
 from keras.models import load_model
 
 # init value
-img_height, img_weight = 224, 244
+img_height, img_weight = 224, 224
 batch_size = 32
 
-image_Test_Path = "./data-set/behavior/test/test.jpg"
-images_Test_Path = "./data-set/behavior/test/"
-weight_Model_Path = "./models/behavior/vgg_16_behavior_1.h5"
+image_test_Path = "./data-set/behavior/test/499.jpg"
+# images_Test_Path = "./data-set/behavior/test/"
+weight_model_path_one = "./models/behavior/vgg16_behavior_1.h5"
+weight_model_path_two = "./models/behavior/vgg16_behavior_2.h5"
 dict_Label = {0: "eat", 1: "noeat"}
 
 # test_datagen = ImageDataGenerator(zoom_range=0.2, horizontal_flip=True)
@@ -24,15 +25,15 @@ dict_Label = {0: "eat", 1: "noeat"}
 # STEP_SIZE_TEST = test_data.n//test_data.batch_size
 
 test_image = image.load_img(
-    image_Test_Path, color_mode='rgb', target_size=(img_height, img_weight))
+    image_test_Path, color_mode='grayscale', target_size=(img_height, img_weight))  # color_mode = rgb or grayscale
 
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 
-plt.imshow(test_image)
+# plt.imshow(test_image)
 
 # load model
-model = load_model(weight_Model_Path)
+model = load_model(weight_model_path_one)
 
 # predict
 output = model.predict(test_image)
