ff8a86732cc251fa57b63749e9d0529de4189fc9
famesensor <sathit.ju@outlook.com>
2021-03-12 00:45:04 +0700

    :hammer: init value

---
+++ expression_main.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    # init value
+    img_height, img_weight = 224, 224
+    channels = 3
+    train_data_path = "./data-set/expression/train/"
+    validation_data_path = "./data-set/expression/validate/"
+    # test_data_path = "./data-set/behavior/test"
+    # test_one_path = "./data-set/behavior/test/test.jpg"
+    check_point_path = "./models/expression/working/"
+    save_weight_vgg16 = "./models/expression/vgg16/vgg16_expression.h5"
+    save_weight_vgg19 = "./models/expression/vgg19/vgg19_expression.h5"
+    save_weight_resnet = "./models/expression/resnet/resnet_expression.h5"
+    save_weight_alexnet = "./models/expression/alexnet/alexnet_expression.h5"
+    save_weight_mobilenet = "./models/expression/mobilenet/mobilenet_expression.h5"
+    batch_size = 32
+    Epochs = 18
+
+    # load data set train,test and validate
+
+    # init vgg16
+
+    # init vgg19
+
+    # init resnet
+
+    # init alexnet
+
+    # init mobilenet
