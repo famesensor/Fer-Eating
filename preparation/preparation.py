904cc0b7687838b040c635fee29a611a67dcba92
famesensor <sathit.ju@outlook.com>
2021-04-24 19:02:12 +0700

    [:hammer:]: add print log and condition

--- preparation/preparation.py
+++ preparation/preparation.py
@@ -12,15 +12,21 @@ def load_data_set(datagen_args: dict, data_set_path: str, size_image: tuple, bat
     data_generator = ImageDataGenerator(**datagen_args)
     data_set = data_generator.flow_from_directory(
         directory=data_set_path, target_size=size_image, class_mode="categorical", batch_size=batch_size)
+    print("[INFO]: load dataset...")
     return data_set
 
 
 def load_image(test_data_path: str, size_image: tuple, color_mode: str):
-    test_image = image.load_img(
-        test_data_path, color_mode=color_mode, target_size=size_image)
+    if len(size_image) != 0:
+        test_image = image.load_img(
+            test_data_path, color_mode=color_mode, target_size=size_image)
+    else:
+        test_image = image.load_img(
+            test_data_path, color_mode=color_mode)
 
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
+    print("[INFO]: load image...")
     return test_image
 
 
