f00e9d151de650d050a0716ce1d2d40e41ec1370
famesensor <sathit.ju@outlook.com>
2021-04-24 19:02:59 +0700

    [:hammer:]: setup compare time of model and compare acc of model

--- expression_compare.py
+++ expression_compare.py
@@ -1,16 +1,18 @@
 import keras
 import numpy as np
 import cv2
+import time
+import glob
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg19 import VGG19
 from keras.applications import ResNet50V2, MobileNetV2
+
 from preparation.preparation import load_image
-from expression.expression_model import setup_network
-from detection.object_detector import face_detect
-from preparation.preparation import load_vdo, resize_image, normalize_image
-from datetime import datetime
+from expression.expression_model import init_model_expression
+from detection.object_detector import face_detect, init_model_face_detect
+from preparation.preparation import load_vdo, resize_image, normalize_image, load_data_set, load_image
 
 if __name__ == "__main__":
     # init value
@@ -23,90 +25,80 @@ if __name__ == "__main__":
     weight_model_vgg19 = "./models/expression/vgg19/vgg19_expression.h5"
     weight_model_resnet = "./models/expression/resnet/resnet_expression.h5"
     weight_model_mobilenet = "./models/expression/mobilenet/mobilenet_expression.h5"
+    config_face = './models/dnn/deploy.prototxt.txt'
+    weight_face = './models/dnn/res10_300x300_ssd_iter_140000_fp16.caffemodel'
+    data_evalute_path = './dataset/validate/'
+    data_time_path = './dataset/unlabel/*.png'
     vdo_path = "./dataset/test/Deep1.MOV"
     include_top = True
     class_num = 8
+    number_frame = 0
     activation = "softmax"
     loss = "categorical_crossentropy"
     dict_exppression = {0: "anger", 1: "contempt", 2: "disgust",
                         3: "fear", 4: "happay", 5: "neutral", 6: "sadness", 7: "surprise"}
+    test_datagen_args = dict(
+        rescale=1./255
+    )
 
-    print("[INFO]: init vgg16 model...")
-    # load model vgg16
-    vgg16 = VGG16(include_top=include_top, input_tensor=None,
-                  input_shape=(img_height, img_weight, channels))
-    model_vgg16 = setup_network(model=vgg16, include_top=include_top,
-                                class_num=class_num, layer_num=19, activation=activation, loss=loss)
-    model_vgg16.load_weights(weight_model_vgg16)
+    # # init model...
+    face_model = init_model_face_detect(config=config_face, weight=weight_face)
 
-    print("[INFO]: init vgg19 model...")
-    # load model vgg19
-    vgg19 = VGG19(include_top=include_top, input_tensor=None,
-                  input_shape=(img_height, img_weight, channels))
-    model_vgg19 = setup_network(model=vgg19, include_top=include_top,
-                                class_num=class_num, layer_num=22, activation=activation, loss=loss)
-    model_vgg19.load_weights(weight_model_vgg19)
+    vgg16 = init_model_expression(weight_path=weight_model_vgg16, types="vgg16", include_top=include_top, img_height=img_height,
+                                  img_weight=img_weight, channels=channels, class_num=class_num, layer_num=19, activation=activation, loss=loss)
 
-    print("[INFO]: init resnet model...")
-    # load model resnet
-    resnet = ResNet50V2(include_top=include_top, input_tensor=None,
-                        input_shape=(img_height, img_weight, channels))
-    model_resnet = setup_network(model=resnet, include_top=include_top,
-                                 class_num=class_num, layer_num=190, activation=activation, loss=loss)
-    model_resnet.load_weights(weight_model_resnet)
+    # vgg19 = init_model_expression(weight_path=weight_model_vgg19, types="vgg19", include_top=include_top, img_height=img_height,
+    #                               img_weight=img_weight, channels=channels, class_num=class_num, layer_num=22, activation=activation, loss=loss)
 
-    print("[INFO]: init moileNet model...")
-    # load model mobilenet
-    mobilenet = MobileNetV2(include_top=include_top, input_tensor=None, input_shape=(
-        img_height, img_weight, channels))
-    model_mobile = setup_network(model=mobilenet, include_top=include_top,
-                                 class_num=class_num, layer_num=154, activation=activation, loss=loss)
-    model_mobile.load_weights(weight_model_mobilenet)
+    # resnet = init_model_expression(weight_path=weight_model_resnet, types="resnet", include_top=include_top, img_height=img_height,
+    #                                img_weight=img_weight, channels=channels, class_num=class_num, layer_num=190, activation=activation, loss=loss)
 
-    print("[INFO]: load vdo...")
-    vdocap = load_vdo(vdo_path=vdo_path)
+    # mobile_net = init_model_expression(weight_path=weight_model_mobilenet, types="mobilenet", include_top=include_top, img_height=img_height,
+    #                                    img_weight=img_weight, channels=channels, class_num=class_num, layer_num=154, activation=activation, loss=loss)
 
-    print("[INFO]: Compare model expression...")
-    test_face = []
-    while True:
-        nth_frame = vdocap.get(cv2.CAP_PROP_POS_FRAMES)
-        ret, frame = vdocap.read()
+    # print("[INFO]: load vdo...")
+    # vdocap = load_vdo(vdo_path=vdo_path)
 
-        if not ret:
-            break
+    print("[INFO]: compare time model expression...")
+    start = datetime.now()
+    startTime = time.time()
+    print("[INFO]: start time predict vgg16: ", start)
+    images = glob.glob(data_time_path)
+    res_predict = []
+    for image_path in images:
+        image = cv2.imread(image_path)
 
         # face dectection
-        face_res = face_detect(image=frame)
+        face_res = face_detect(net=face_model, image=image)
 
         # preparation data for expression model
         face_res = resize_image(
-            image=face_res, size_image=(224, 224))
+            image=face_res, size_image=(img_height, img_weight))
         face_res = normalize_image(image=face_res)
         face_res = np.expand_dims(face_res, axis=0)
-        test_face.append(face_res)
 
-    start = datetime.now()
-    print("[INFO]: start time predict vgg16: ", start)
-    res_vgg16 = model_vgg16.predict(test_face)
+        res_predict.append(vgg16.predict(face_res))
+        # res_predict.append(vgg19.predict(face_res))
+        # res_predict.append(resnet.predict(face_res))
+        # res_predict.append(mobile_net.predict(face_res))
+
+    # Time elapsed
+    end = time.time()
     duration = datetime.now() - start
     print("[INFO]: time predict vgg16 : ", duration)
+    seconds = end - startTime
+    fps = len(images)/seconds
+    print("[INFO]: Estimated frames per second : {0}".format(fps))
 
-    start = datetime.now()
-    print("[INFO]: start time predict vgg19: ", start)
-    res_vgg19 = model_vgg19.predict(test_face)
-    duration = datetime.now() - start
-    print("[INFO]: time predict vgg19 : ", duration)
+    # print("[INFO]: compare evaluate model expression...")
+    # test_data = load_data_set(
+    #     test_datagen_args, test_data_path, (img_height, img_weight), batch_size)
 
-    start = datetime.now()
-    print("[INFO]: start time predict resnet: ", start)
-    res_resnet = model_resnet.predict(test_face)
-    duration = datetime.now() - start
-    print("[INFO]: time predict resnet : ", duration)
+    # # vgg16_evaluate[0] is loss, vgg16_evaluate[1] is accreency
+    # vgg16_evaluate = vgg16.evaluate(test_data)
 
-    start = datetime.now()
-    print("[INFO]: start time predict mobile: ", start)
-    res_mobile = model_mobile.predict(test_face)
-    duration = datetime.now() - start
-    print("[INFO]: time predict mobile : ", duration)
-    
-    
+    # vgg19_evaluate = vgg19.evaluate(test_data)
+
+    # resnet_evaluate = resnet.evaluate(test_data)
+
+    # mobile_evaluate = mobile_net.evaluate(test_data)
