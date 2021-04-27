cf144e68ec3ed365796c92930bb6892112156fb4
famesensor <sathit.ju@outlook.com>
2021-04-27 22:57:56 +0700

    [:hammer:]: setup train behavior model

--- behavior_main.py
+++ behavior_main.py
@@ -1,21 +1,27 @@
 import numpy as np
 
-from preparation import load_data_set, load_image
-from behavior.behavior_model import init_model_vgg16, setup_architechture_vgg16, setup_architechture_vgg16_2, train_model_vgg16, plot_result_train_model
+from preparation.preparation import load_data_set, load_image
+from behavior.behavior_model import init_model_train_behavior, train_model, plot_result_train_model
 
 if __name__ == "__main__":
     # init value
     img_height, img_weight = 224, 224
     channels = 3
-    train_data_path = "./data-set/behavior/train"
-    validation_data_path = "./data-set/behavior/validate"
-    test_data_path = "./data-set/behavior/test"
-    test_one_path = "./data-set/behavior/test/test.jpg"
-    check_point_path = "./model/behavior/"
-    save_weight_one_path = "./models/behavior/vgg16_behavior_1.h5"
-    save_weight_two_path = "./models/behavior/vgg16_behavior_2.h5"
+    train_data_path = "./dataset/behavior/train"
+    validation_data_path = "./dataset/behavior/validate"
+    test_data_path = "./dataset/behavior/test"
+    # test_one_path = "./data-set/behavior/test/test.jpg"
+    check_point_path = "./models/behavior/working/"
+    save_weight_vgg16_path = "./models/behavior/vgg16/vgg16_behavior.h5"
+    save_weight_vgg19_path = "./models/behavior/vgg19/vgg19_behavior.h5"
+    save_weight_resnet_path = "./models/behavior/resnet/resnet_behavior.h5"
+    save_weight_mobilenet_path = "./models/behavior/mobilenet/mobilenet_behavior.h5"
     batch_size = 32
     Epochs = 18
+    include_top = True
+    class_num = 2
+    activation = "softmax"
+    loss = "categorical_crossentropy"
     dict_label = {0: "eat", 1: "noeat"}
     train_datagen_args = dict(
         rotation_range=20,
@@ -33,37 +39,33 @@ if __name__ == "__main__":
         train_datagen_args, train_data_path, (img_height, img_weight), batch_size)
     validation_data = load_data_set(
         train_datagen_args, validation_data_path, (img_height, img_weight), batch_size)
-    test_data = load_data_set(
-        test_datagen_args, test_data_path, (img_height, img_weight), batch_size)
+    # test_data = load_data_set(
+    #     test_datagen_args, test_data_path, (img_height, img_weight), batch_size)
 
     STEP_SIZE_TRAIN = train_data.n//train_data.batch_size
     STEP_SIZE_VALID = validation_data.n//validation_data.batch_size
-    STEP_SIZE_TEST = test_data.n // test_data.batch_size
+    # STEP_SIZE_TEST = test_data.n // test_data.batch_size
 
-    # init model vgg 1
-    model_1 = init_model_vgg16(
-        include_top=False, input_tensor=None, input_shape=(img_height, img_weight, channels))
+    vgg16 = init_model_train_behavior(types="vgg16", include_top=include_top, img_height=img_height,
+                                      img_weight=img_weight, channels=channels, class_num=class_num, layer_num=19, activation=activation, loss=loss)
+    vgg16_res, history_vgg16 = train_model(checkpoint_path=check_point_path+"vgg16_best.h5", save_weights_path=save_weight_vgg16_path, model=vgg16, train_data=train_data,
+                                           validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs_train=Epochs)
+    plot_result_train_model(history_vgg16, "vgg16")
 
-    model_1 = setup_architechture_vgg16(model_1)
-    model_train_1, history_1 = train_model_vgg16(checkpoint_path=check_point_path, save_weights_path=save_weight_one_path, model=model_1,
-                                                 train_data=train_data, validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs=Epochs)
-    plot_result_train_model(history_1)
+    vgg19 = init_model_train_behavior(types="vgg19", include_top=include_top, img_height=img_height,
+                                      img_weight=img_weight, channels=channels, class_num=class_num, layer_num=22, activation=activation, loss=loss)
+    vgg19_res, history_vgg19 = train_model(checkpoint_path=check_point_path+"vgg19_best.h5", save_weights_path=save_weight_vgg19_path, model=vgg19, train_data=train_data,
+                                           validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs_train=Epochs)
+    plot_result_train_model(history_vgg19, "vgg19")
 
-    # init model vgg 2
-    model_2 = init_model_vgg16(
-        include_top=True, input_tensor=None, input_shape=(img_height, img_weight, channels))
-    model_2 = setup_architechture_vgg16_2(model_2)
-    model_train_2, history_2 = train_model_vgg16(checkpoint_path=check_point_path, save_weights_path=save_weight_two_path, model=model_2,
-                                                 train_data=train_data, validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs_train=Epochs)
-    plot_result_train_model(history_2)
+    resnet = init_model_train_behavior(types="resnet", include_top=include_top, img_height=img_height,
+                                       img_weight=img_weight, channels=channels, class_num=class_num, layer_num=190, activation=activation, loss=loss)
+    resnet_res, history_resnet = train_model(checkpoint_path=check_point_path+"resnet_best.h5", save_weights_path=save_weight_resnet_path, model=resnet, train_data=train_data,
+                                             validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs_train=Epochs)
+    plot_result_train_model(history_resnet, "resnet")
 
-    # # test model
-    # test_image = load_image(
-    #     test_one_path, (img_height, img_weight), color_mode='rgb')
-    # result_model_1 = model_train_1.predict(test_image)
-    # result_model_2 = model_train_2.predict(test_image)
-
-    # res_model_1 = np.argmax(result_model_1)
-    # res_model_2 = np.argmax(result_model_2)
-    # print("The predicted output model1 is :", dict_label[res_model_1])
-    # print("The predicted output model2 is :", dict_label[res_model_2])
+    mobile_net = init_model_train_behavior(types="mobilenet", include_top=include_top, img_height=img_height,
+                                           img_weight=img_weight, channels=channels, class_num=class_num, layer_num=154, activation=activation, loss=loss)
+    mobile_net_res, history_mobile_net = train_model(checkpoint_path=check_point_path+"mobilenet_best.h5", save_weights_path=save_weight_mobilenet_path, model=mobile_net, train_data=train_data,
+                                                     validation_data=validation_data, step_size_train=STEP_SIZE_TRAIN, step_size_valid=STEP_SIZE_VALID, epochs_train=Epochs)
+    plot_result_train_model(history_mobile_net, "mobilenet")
