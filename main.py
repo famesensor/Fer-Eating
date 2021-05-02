0b7665c192d9d3c72dbcdb1cf14176b7cad6f2b9
famesensor <sathit.ju@outlook.com>
2021-05-02 13:25:04 +0700

    [:hammer:]: add export frame to img, fix init model behavior

--- main.py
+++ main.py
@@ -11,46 +11,6 @@ from preparation.preparation import load_image, load_vdo, resize_image, normaliz
 from behavior.behavior_model import init_model_behavior
 from expression.expression_model import init_model_expression
 
-# init values...
-config_person = './models/yolo/yolov4.cfg'
-weight_person = './models/yolo/yolov4.weights'
-config_face = './models/dnn/deploy.prototxt.txt'
-weight_face = './models/dnn/res10_300x300_ssd_iter_140000_fp16.caffemodel'
-weight_behavior = "./models/behavior/vgg16_behavior_1.h5"
-weight_expression = "./models/expression/vgg16/vgg16_expression.h5"
-include_top = True
-class_num = 8
-number_frame = 0
-img_height, img_weight = 224, 224
-channels = 3  # 3 RGB
-batch_size = 32
-activation = "softmax"
-loss = "categorical_crossentropy"
-every_n_frame = 5
-flag_eat = False
-number_eat = 0
-count_step = 0
-dict_exppression = {0: "anger", 1: "contempt", 2: "disgust",
-                    3: "fear", 4: "happay", 5: "neutral", 6: "sadness", 7: "surprise"}
-dict_behavior = {0: "eat", 1: "noeat"}
-behavior_res = []
-expression_res = []
-frame_start_eat = 0
-frame_end_eat = 0
-img_height, img_width = 224, 224
-
-# init model...
-person_model = init_model_person_detect(
-    config=config_person, weight=weight_person)
-face_model = init_model_face_detect(config=config_face, weight=weight_face)
-behavior_model = init_model_behavior(weight_path=weight_behavior)
-expression_model = init_model_expression(weight_path=weight_expression, types="vgg16", include_top=include_top, img_height=img_height,
-                                         img_weight=img_weight, channels=channels, class_num=class_num, layer_num=19, activation=activation, loss=loss)
-
-# load dataset...
-vdo_path = "./dataset/test/Deep1.MOV"
-vdocap = load_vdo(vdo_path=vdo_path)
-
 
 # def expression_process(image: list):
 #     # person detection
@@ -87,6 +47,46 @@ vdocap = load_vdo(vdo_path=vdo_path)
 
 
 if __name__ == "__main__":
+    # init values...
+    config_person = './models/yolo/yolov4.cfg'
+    weight_person = './models/yolo/yolov4.weights'
+    config_face = './models/dnn/deploy.prototxt.txt'
+    weight_face = './models/dnn/res10_300x300_ssd_iter_140000_fp16.caffemodel'
+    weight_behavior = "./models/behavior/vgg16_behavior_2.h5"
+    weight_expression = "./models/expression/vgg16/vgg16_expression.h5"
+    include_top = True
+    class_num = 8
+    number_frame = 0
+    img_height, img_weight = 224, 224
+    channels = 3  # 3 RGB
+    batch_size = 32
+    activation = "softmax"
+    loss = "categorical_crossentropy"
+    every_n_frame = 5
+    flag_eat = False
+    number_eat = 0
+    count_step = 0
+    dict_exppression = {0: "anger", 1: "contempt", 2: "disgust",
+                        3: "fear", 4: "happay", 5: "neutral", 6: "sadness", 7: "surprise"}
+    dict_behavior = {0: "eat", 1: "noeat"}
+    behavior_res = []
+    expression_res = []
+    frame_start_eat = 0
+    frame_end_eat = 0
+    img_height, img_width = 224, 224
+
+    # init model...
+    person_model = init_model_person_detect(
+        config=config_person, weight=weight_person)
+    face_model = init_model_face_detect(config=config_face, weight=weight_face)
+    behavior_model = init_model_behavior(weight_path=weight_behavior, types="vgg16", include_top=include_top, img_height=img_height,
+                                         img_weight=img_weight, channels=channels, class_num=2, layer_num=19, activation=activation, loss=loss)
+    expression_model = init_model_expression(weight_path=weight_expression, types="vgg16", include_top=include_top, img_height=img_height,
+                                             img_weight=img_weight, channels=channels, class_num=class_num, layer_num=19, activation=activation, loss=loss)
+
+    # load dataset...
+    vdo_path = "./dataset/test/Deep1.MOV"
+    vdocap = load_vdo(vdo_path=vdo_path)
     index = 0
     print("[INFO]: ID of main process: {}".format(os.getpid()))
 
@@ -127,6 +127,11 @@ if __name__ == "__main__":
                     number_eat = 1
                     every_n_frame = 1
                     frame_start_eat = nth_frame
+
+                    file_name = './export/export_first_frame_eat_' + \
+                        str(nth_frame)+'.jpg'
+                    cv2.imwrite(file_name, frame)
+
             if flag_eat and number_eat == 1:
                 print("[INFO]: after first eating...")
                 count_step += 1
