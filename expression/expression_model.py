c1dde9bb7ccf45e19d5069e5e585b5100d653261
famesensor <sathit.ju@outlook.com>
2021-06-21 22:56:54 +0700

    [:recycle:]: refactor code

--- expression/expression_model.py
+++ expression/expression_model.py
@@ -1,6 +1,5 @@
 import keras
 import numpy as np
-import matplotlib.pyplot as plt
 
 from datetime import datetime
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
@@ -14,80 +13,6 @@ from keras.models import Model
 from keras import models, layers
 from keras.layers.normalization import BatchNormalization
 from keras.layers import Activation, Dropout, Conv2D, MaxPooling2D
-
-
-def init_model_vgg16(include_top: bool, input_tensor, input_shape: tuple) -> Model:
-    model = VGG16(include_top=include_top, weights='imagenet',
-                  input_tensor=None, input_shape=input_shape)
-    return model
-
-
-def init_model_vgg19(include_top: bool, input_tensor, input_shape: tuple) -> Model:
-    model = VGG19(include_top=include_top, weights='imagenet',
-                  input_tensor=None, input_shape=input_shape)
-    return model
-
-
-def init_model_resnet50v2(include_top: bool, input_tensor, input_shape: tuple) -> Model:
-    model = ResNet50V2(include_top=include_top, weights='imagenet',
-                       input_tensor=None, input_shape=input_shape)
-    return model
-
-
-def init_model_mobilenet(include_top: bool, input_tensor, input_shape: tuple, minimalistic: bool, alpha: float, dropout_rate: float) -> Model:
-    model = MobileNetV2(include_top=include_top, weights='imagenet',
-                        input_tensor=None, input_shape=input_shape)
-    return model
-
-
-def init_model_alexnet():
-    model = models.Sequential()
-
-    # Convolutional Layer
-    # 1st Convolutional Layer
-    model.add(Conv2D(filters=96, kernel_size=(11, 11), strides=(
-        4, 4), activation='relu', input_shape=(227, 227, 3)))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    # 2nd Convolutional Layer
-    model.add(Conv2D(filters=256, kernel_size=(5, 5),
-                     strides=(1, 1), activation='relu', padding="same"))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    # 3rd Convolutional Layer
-    model.add(Conv2D(filters=384, kernel_size=(3, 3),
-                     strides=(1, 1), activation='relu', padding="same"))
-    model.add(BatchNormalization())
-
-    # 4th Convolutional Layer
-    model.add(Conv2D(filters=384, kernel_size=(1, 1),
-                     strides=(1, 1), activation='relu', padding="same"))
-    model.add(BatchNormalization())
-
-    # 5th Convolutional Layer
-    model.add(Conv2D(filters=256, kernel_size=(1, 1),
-                     strides=(1, 1), activation='relu', padding="same"))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    # Fully Connected layer
-    # 1st Fully Connected Layer
-    model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-
-    # 2nd Fully Connected Layer
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-
-    # 3rd Fully Connected Layer
-    model.add(Dense(8, activation='softmax'))
-
-    model.compile(loss="categorical_crossentropy",
-                  optimizer=optimizers.Adam(), metrics=['accuracy'])
-    return model
 
 
 def init_model_train_expression(types: str, include_top: bool, img_height: int, img_width: int, channels: int, class_num: int, layer_num: int, activation: str, loss: str, dropout=0.2) -> Model:
@@ -169,25 +94,6 @@ def train_model(checkpoint_path: str, save_weight_path: str, model: Model, train
 def evaluate_model(model: Model, test_data_set):
     score = model.evaluate(test_data_set)
     return score
-
-
-def plot_result_train_model(history, model_name: str):
-    plt.plot(history.history["accuracy"])
-    plt.plot(history.history['val_accuracy'])
-    plt.title(model_name)
-    plt.ylabel("Accuracy")
-    plt.xlabel("Epoch")
-    plt.legend(["Accuracy", "Validation Accuracy"])
-    plt.show()
-
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title(model_name)
-    plt.ylabel("Loss")
-    plt.xlabel("Epoch")
-    plt.legend(["loss", "Validation Loss"])
-    plt.show()
-    return
 
 
 def init_model_expression(weight_path: str, types: str, include_top: bool, img_height: int, img_weight: int, channels: int, class_num: int, layer_num: int, activation: str, loss: str) -> Model:
